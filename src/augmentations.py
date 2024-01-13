b234e4d61c56152b0dcc68591326e293d027830b
DimYun <Dm.Yunovidov@gmail.com>
2024-01-13 15:45:14 +0200

    hotfix: set augmentation to separate yaml config file

--- src/augmentations.py
+++ src/augmentations.py
@@ -15,15 +15,10 @@ def get_transforms(
         transforms.append(albu.Resize(height=height, width=width))
 
     if augmentations:
+        loaded_transform = albu.load('../configs/augmentations_conf.yaml', data_format='yaml')
+        transforms_list = loaded_transform.transforms
         transforms.extend(
-            [
-                albu.HorizontalFlip(p=0.5),
-                albu.VerticalFlip(p=0.5),
-                albu.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=0.5),
-                albu.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
-                albu.ShiftScaleRotate(),
-                albu.GaussianBlur(),
-            ],
+            transforms_list,
         )
 
     if postprocessing:
